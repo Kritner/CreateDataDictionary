a80a06d1efd2e55fb9c7c83fc72c6df97f8ce54a
kritner <kritner@gmail.com>
2016-04-14 13:00:37 -0400

    - Added ExcludeFromCodeCoverage to missed unit test classes

--- CreateDataDictionary.Business.Tests/Models/TableColumnInfoTests.cs
+++ CreateDataDictionary.Business.Tests/Models/TableColumnInfoTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using CreateDataDictionary.Business.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,6 +11,7 @@ namespace CreateDataDictionary.Business.Tests.Models
     /// Tests for TableColumnInfo
     /// </summary>
     [TestClass]
+    [ExcludeFromCodeCoverage]
     public class TableColumnInfoTests
     {
 
